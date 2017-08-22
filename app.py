19a075049b2a46d99383e0c3ea154c1887de33ef
xbb233 <31240327+xbb233@users.noreply.github.com>
2017-08-22 17:25:30 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Cool!</h1>', content_type='text/html')
+    
+def foo(request):
+    text = '<h1>%s,%s!</h1>' % (request.match_info['file'], request.match_info['name'])
+    return web.Response(body=text.encode('utf-8'), content_type='text/html')
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/{file}/{name}', foo)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
---
+++ orm.py
@@ -0,0 +1,20 @@
+import asyncio
+import logging; logging.basicConfig(level=logging.INFO)
+import aiomysql
+
+async def create_pool(loop, **kw):#创建连接池
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop    
+    )
+    
